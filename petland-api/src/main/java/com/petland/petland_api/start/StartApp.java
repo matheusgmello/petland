de3407efda9ab7da7f724c74147e196b1db0964d
Matheus <matheusgmello1@gmail.com>
2024-06-27 14:04:18 -0300

    feat: mapeando entidades na API

--- petland-api/src/main/java/com/petland/petland_api/start/StartApp.java
+++ petland-api/src/main/java/com/petland/petland_api/start/StartApp.java
@@ -1,13 +1,23 @@
 package com.petland.petland_api.start;
 
+import com.petland.petland_api.model.Cadastro;
+import com.petland.petland_api.repository.CadastroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartApp implements ApplicationRunner {
+
+    @Autowired
+    private CadastroRepository repository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Hello from StartApp");
+        Cadastro matheus = new Cadastro();
+        matheus.setId(1);
+        matheus.setNome("Matheus Mello");
+        repository.save(matheus);
     }
 }
